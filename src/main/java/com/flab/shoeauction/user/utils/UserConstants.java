e8b105827aedf0d47bcb1149e4aef29ed47d107e
jungkh405@naver.com <jungkh405@naver.com>
2021-01-19 15:09:37 +0900

    update : 휴대폰 인증 로직 변경
    1.기존 session 방식에서 concurrenthashmap을 사용하는 방식으로 변경

--- src/main/java/com/flab/shoeauction/user/utils/UserConstants.java
+++
@@ -1,8 +0,0 @@
-package com.flab.shoeauction.user.utils;
-
-public class UserConstants {
-
-  public static final String API_KEY = "";
-  public static final String API_SECRET = "";
-  public static final int NUMBER_GENERATION_COUNT = 6;
-}
