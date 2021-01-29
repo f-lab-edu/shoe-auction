9eb7c00f370e326929d78f3a6d6bca3707f0d6aa
Hosick <ghtjr5000@naver.com>
2021-01-29 16:59:19 +0900

    Merge branch

--- src/main/java/com/flab/shoeauction/Application.java
+++ src/main/java/com/flab/shoeauction/Application.java
@@ -2,13 +2,9 @@ package com.flab.shoeauction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-<<<<<<< HEAD
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
-=======
-
->>>>>>> 95e4aa9a1ec66262028b338d962272aea0e6598f
 @SpringBootApplication
 public class Application {
 
