37ab4406bee535180289a0e7d781ce9ae1423bed
Hosick <ghtjr5000@naver.com>
2021-03-02 06:00:02 +0900

    feat: 리뷰 반영
    -properties 바인딩을 수정자에서 생성자를 사용하는 방식으로 변경
    
    issue number: #30

--- src/main/java/com/flab/shoeauction/common/config/CacheProperties.java
+++ src/main/java/com/flab/shoeauction/common/config/CacheProperties.java
@@ -1,8 +1,8 @@
 package com.flab.shoeauction.common.config;
 
+import java.util.HashMap;
 import java.util.Map;
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
@@ -11,18 +11,9 @@ import org.springframework.stereotype.Component;
  * 트래픽에 따라 캐시 별 만료기간을 쉽게 변경할 수 있도록 properties에서 cache 이름별 만료 기간을 Map에 바인딩한다.
  */
 @Getter
-@Setter
 @Component
-@ConfigurationProperties(prefix = "redis-cache-constants")
+@ConfigurationProperties(prefix = "cache.redis")
 public class CacheProperties {
 
-    private Map<String, Long> ttl;
-
-    @Getter
-    @Setter
-    public static class CacheNameAndTimeout {
-
-        private String name;
-        private String Timeout;
-    }
+    private final Map<String, Long> ttl = new HashMap<>();
 }
