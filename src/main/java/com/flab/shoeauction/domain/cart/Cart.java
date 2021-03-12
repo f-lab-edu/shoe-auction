9505d51cc8c6da0d88d9382010cd3c673630ac95
Jeong ki hyuk <39195377+ROMANIAPEOPLE@users.noreply.github.com>
2021-03-12 21:18:11 +0900

    [#54] feat : 위시리스트 도메인 설계 (#55)
    * [#54] feat : 위시리스트 도메인 설계
    
    1. Cart
    2. CartProduct
    3. User
    
     #54 : 위시리스트(CART) 모델링
    
    * refactor : 리뷰 반영
    
    1. cart와 user 일대일 양방향 매핑으로 수정

---
+++ src/main/java/com/flab/shoeauction/domain/cart/Cart.java
@@ -0,0 +1,28 @@
+package com.flab.shoeauction.domain.cart;
+
+import com.flab.shoeauction.domain.users.user.User;
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Cart {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @OneToOne(mappedBy = "user")
+    private User user;
+
+    @OneToMany(mappedBy = "cart")
+    private Set<CartProduct> wishList = new HashSet<>();
+}
