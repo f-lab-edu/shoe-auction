e2a22773e7a430e8941400deee49805ccb13103e
jungkh405@naver.com <jungkh405@naver.com>
2021-03-19 18:11:48 +0900

    merge

--- src/main/java/com/flab/shoeauction/controller/BrandApiController.java
+++ src/main/java/com/flab/shoeauction/controller/BrandApiController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.multipart.MultipartFile;
-
+//CI
 @RequiredArgsConstructor
 @RequestMapping("/brands")
 @Controller
