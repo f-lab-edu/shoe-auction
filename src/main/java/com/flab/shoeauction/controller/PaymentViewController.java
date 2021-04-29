f18826a1d00276eee4bd9ace747b0e857c774bd1
jungkh405@naver.com <jungkh405@naver.com>
2021-04-29 21:54:47 +0900

    [#106] refactor : 리뷰 반영
    1. 뷰 컨트롤러 이름 변경
    
    issue Number : #106

--- src/main/java/com/flab/shoeauction/controller/PaymentViewController.java
+++ src/main/java/com/flab/shoeauction/controller/PaymentViewController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class ViewController {
+public class PaymentViewController {
 
     @GetMapping("/points/charge")
     public String paymentPage() {
