da53d5682602a73b96fa8cacc6efbe47619eb4e3
jungkh405@naver.com <jungkh405@naver.com>
2021-05-07 21:12:06 +0900

    chore : deploy test

--- src/main/java/com/flab/shoeauction/controller/AdminApiController.java
+++ src/main/java/com/flab/shoeauction/controller/AdminApiController.java
@@ -32,7 +32,7 @@ public class AdminApiController {
 
     @GetMapping("/temp")
     public String temp() {
-        return "ㅁㅎㄴ";
+        return "check deploy";
     }
 
     @LoginCheck(authority = UserLevel.ADMIN)
