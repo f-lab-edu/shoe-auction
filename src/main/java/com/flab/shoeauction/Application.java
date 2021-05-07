b77be0c0bc252c573b959fd07c6595fe669bfb12
jungkh405@naver.com <jungkh405@naver.com>
2021-05-08 02:29:07 +0900

    배포 테스트

--- src/main/java/com/flab/shoeauction/Application.java
+++ src/main/java/com/flab/shoeauction/Application.java
@@ -1,13 +1,11 @@
 package com.flab.shoeauction;
 
-import com.flab.shoeauction.common.config.ShutdownHookConfiguration;
 import com.flab.shoeauction.common.properties.AppProperties;
 import com.flab.shoeauction.common.properties.AwsProperties;
 import com.flab.shoeauction.common.properties.CacheProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableConfigurationProperties(value = {AppProperties.class, CacheProperties.class,
