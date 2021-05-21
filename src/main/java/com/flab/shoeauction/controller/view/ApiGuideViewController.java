b72329412700eb1aa80f9d19db35126e7d8b30f7
jungkh405@naver.com <jungkh405@naver.com>
2021-05-22 01:51:19 +0900

    chore : API docs URL 변경

--- src/main/java/com/flab/shoeauction/controller/view/ApiGuideViewController.java
+++ src/main/java/com/flab/shoeauction/controller/view/ApiGuideViewController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ApiGuideViewController {
 
-    @GetMapping("docs")
+    @GetMapping("/docs")
     public String apiGuideDocs() {
         return "api-guide";
     }
